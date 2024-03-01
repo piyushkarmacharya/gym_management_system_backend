9ae2c940cb3b843d9e782dca438b609cf4beb93c
piyushkarmacharya <Karmacharyapiyush65@gmail.com>
2024-03-01 09:36:12 +0545

    created api for staff registration

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -10,4 +10,20 @@ class StaffController extends Controller
         $data=Staff::where("email",$email)->get();
         return response()->json($data);
     }
+    public function create(Request $req){
+        
+        $Staff=Staff::create([
+            'first name'=>$req->name,
+            'last name'=>"default for now",
+            'gender'=>$req->gender,
+            'email'=>$req->email,
+            'phone number'=>$req->contact_number,
+            'address'=>$req->address,
+            'password'=>$req->password,
+        ]);
+
+        return response()->json(['message'=>"Successfully inserted"],200);
+    
+    
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,5 @@ Route::get('Staff/login/{email}',[StaffController::class,'read']);
 Route::post('Member/register',[MemberController::class,'create']);
 Route::get('Member/details',[MemberController::class,'read']);
 Route::get('Member/details/{email}',[MemberController::class,'readSingle']);
+
+Route::post('Staff/register',[StaffController::class,'create']);
