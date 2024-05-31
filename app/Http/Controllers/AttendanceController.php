457218ef0d1daf447620f575e58cc677c3d2254e
piyushkarmacharya <Karmacharyapiyush65@gmail.com>
2024-05-31 08:39:48 +0545

    created api to return total present day of a member

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -6,14 +6,35 @@ use App\Models\Attendance;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 
 class AttendanceController extends Controller
 {
+    public function getTotalPresent($date,Request $req){
+        $totalPresent=Attendance::where('date',$date)->count();
+        return response()->json(["totalPresent" => $totalPresent]);
+
+    }
+    public function getMemberTotalPresent($mid,Request $req){
+        $currentMonth = Carbon::now()->month;
+        $currentYear = Carbon::now()->year;
+
+        $totalPresent = Attendance::where('mid', $mid)
+            ->whereMonth('date', $currentMonth)
+            ->whereYear('date', $currentYear)
+            ->count();
+
+        return response()->json(["totalPresent" => $totalPresent]);
+
+       
+
+    }
+
     public function attend(Request $req)
     {
         $current_date = date("Y-m-d");
-        $attendance = Attendance::where('mid', $req->mid)->orderBy('created_at', 'desc')->first();
+        $attendance = Attendance::where('mid', $req->mid)->orderBy('date', 'desc')->first();
 
         if ($attendance) {
             $last_attendance_date = $attendance->date;
