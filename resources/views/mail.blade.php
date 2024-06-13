810798a55a4c0f6e527599e4ad4d02ecc7ab6ca7
piyushkarmacharya <Karmacharyapiyush65@gmail.com>
2024-06-13 09:04:38 +0545

    changed blade format to remove mail from spam

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+
+<html>
+
 <body>
     <p>Hello {{ $name }},</p>
     <p>Welcome to Club Desperado. Following are your credentials for Club Desperado's application which you can download from playstore.</p>
     <br><p>Email : {{ $email }}</p>
-    <p>Password : {{ $password }}</p>
+    <p>Pass : {{ $password }}</p>
     <br>
     <p>Best,</p>
     <p>Club Desperado</p>
